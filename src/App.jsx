6df3ed559ef84fb50a3810e0d2be9126d55316f5
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-03-12 14:45:07 +0200

    add lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import SharedLayout from "./components/SharedLayout/SharedLayout";
-import Home from "./pages/Home/Home";
-import Person from "./pages/Person/Person";
+
+const HomePage = lazy(() => import("../pages/Home/Home"));
+const PersonPage = lazy(() => import("../pages/Person/Person"));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/:id" element={<Person />} />
+        <Route index element={<HomePage />} />
+        <Route path="/:id" element={<PersonPage />} />
       </Route>
     </Routes>
   );
